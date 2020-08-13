4ea4e5c52cc8d36802f0e6856de3d7b7ff454c0e
Tutorial Mania <50166038+Tutorial-Mania@users.noreply.github.com>
2020-08-13 17:05:45 +0600

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,12 +1,20 @@
 #!bin/bash
 
-apt-get install wget
+if [[ -e ngrok ]]; then
+echo "Done"
+elif [[ -e ngrok-2.2.8-linux-arm.zip ]]; then
 apt-get install unzip
-wget https://bin.equinox.io/a/e93TBaoFgZw/ngrok-2.2.8-linux-arm.zip
-if [[ -e ngrok-2.2.8-linux-arm.zip ]]; then
 unzip ngrok-2.2.8-linux-arm.zip
 chmod +x ngrok
 rm -rf ngrok-2.2.8-linux-arm.zip
+echo "Done"
+elif [[ x != ngrok-2.2.8-linux-arm.zip ]]; then
+apt-get install wget
+wget https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm.zip
+unzip ngrok-2.2.8-linux-arm.zip
+chmod +x ngrok
+rm -rf ngrok-2.2.8-linux-arm.zip
+echo "Done"
 else
 printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
 exit 1
