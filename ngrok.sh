de7a9aec9eefe512eed78699a3beb276203380ff
Tutorial Mania <50166038+Tutorial-Mania@users.noreply.github.com>
2020-08-14 13:48:34 +0600

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,21 +1,16 @@
 #!bin/bash
 
-menu() {
-
-echo "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m [1] - Download Ngrok Letest Version\e[0m"
-echo "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m [2] - Download Ngrok Version 2.2.8\e[0m"
-
+echo "  [1] - Download Ngrok Letest Version"
+echo "  [2] - Download Ngrok Version 2.2.8"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' option
 
 if [[ $option == 1 ]]; then 
-bash /ngrok/ngrok-stable.sh
+bash ngrok/ngrok-stable.sh
 
 elif [[ $option == 2 ]]; then
-bash /ngrok/ngrok-2.2.8.sh
+bash ngrok/ngrok-2.2.8.sh
 
 else
-echo "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m Please cheack your internet\e[0m\n"
+echo " Inviald Option"
 exit 1
-menu
 fi
-}
