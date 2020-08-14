7c1826504412c2968d82945de0606e0383757c88
Tutorial Mania <50166038+Tutorial-Mania@users.noreply.github.com>
2020-08-14 13:09:20 +0600

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,21 +1,21 @@
 #!bin/bash
 
-if [[ -e ngrok ]]; then
-echo "Done"
-elif [[ -e ngrok-2.2.8-linux-arm.zip ]]; then
-apt-get install unzip -y
-unzip ngrok-2.2.8-linux-arm.zip
-chmod +x ngrok
-rm -rf ngrok-2.2.8-linux-arm.zip
-echo "Done"
-elif [[ ! -e ngrok-2.2.8-linux-arm.zip ]]; then
-apt-get install wget -y
-wget https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm.zip
-unzip ngrok-2.2.8-linux-arm.zip
-chmod +x ngrok
-rm -rf ngrok-2.2.8-linux-arm.zip
-echo "Done"
+menu() {
+
+echo "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m [1] - Download Ngrok Letest Version\e[0m"
+echo "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m [2] - Download Ngrok Version 2.2.8\e[0m"
+
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' option
+
+if [[ $option == 1 ]]; then 
+bash /.ngrok/ngrok-stable.sh
+
+elif [[ $option == 2 ]]; then
+bash /.ngrok/ngrok-2.2.8.sh
+
 else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m Please cheack your internet\e[0m\n"
+echo "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m Please cheack your internet\e[0m\n"
 exit 1
+menu
 fi
+}
